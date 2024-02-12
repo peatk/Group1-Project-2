4bf2c8908ee0a1683b53b3578ab1299949e338c5
peatk <peterklinecp@gmail.com>
2024-02-12 12:37:55 -0800

    updated with hok's code

--- app.js
+++ app.js
@@ -1,44 +1,126 @@
-// JSON data containing latitude and longitude information
-const jsonData = [
-    {
-        "Author": "Àbíké-Íyímídé, Faridah",
-        "Latitude": 27.994402,
-        "Longitude": -81.760254,
-        "Title": "Ace of Spades",
-        "Date_of_Challenge_or_Removal": 1635724800000,
-        "District": "Indian River County School District",
-        "Origin_of_Challenge": "Administrator",
-        "State": "Florida",
-        "Type_of_Ban": "Banned in Libraries and Classrooms"
-        // Other properties...
-    },
-    // Other locations...
-];
+console.log(libraryData);
 
-// Initialize Leaflet map
-const map = L.map('map').setView([37.8, -96], 4); // Centered on the US
+function createFeatures(libraryData) {
 
-// Add a tile layer to the map
+    let banByState = {};
+
+    libraryData.forEach(item => {
+        if (banByState[item.State]) {
+            banByState[item.State].bookCount += 1;
+        } else {
+            banByState[item.State] = {
+                State: item.State,
+                bookCount: 1,
+                lat: item.Latitude,
+                lng: item.Longitude
+            };
+        }
+    });
+
+    console.log(banByState);
+    return banByState;
+
+}
+
+
+let banByState = createFeatures(libraryData);
+init(banByState);
+
+let myMap = L.map("map", {
+    center: [39.833, -98.583],
+    zoom: 5
+});
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
+}).addTo(myMap);
 
-// Iterate over JSON data and add markers to the map for each location
-jsonData.forEach(location => {
-    const { Latitude, Longitude, Author, Title, Date_of_Challenge_or_Removal, District, Origin_of_Challenge, State, Type_of_Ban } = location;
+L.geoJson(statesData).addTo(myMap);
 
-    // Create a popup with information about the book
-    const popupContent = `
-        <b>${Title}</b><br>
-        Author: ${Author}<br>
-        Date of Challenge or Removal: ${new Date(Date_of_Challenge_or_Removal).toLocaleDateString()}<br>
-        District: ${District}<br>
-        Origin of Challenge: ${Origin_of_Challenge}<br>
-        State: ${State}<br>
-        Type of Ban: ${Type_of_Ban}
-    `;
+function getColor(bookCount) {
+    return bookCount > 80 ? '#08519c' :
+           bookCount > 40 ? '#3182bd':
+           bookCount > 20 ? '#6baed6':
+           bookCount > 10 ? '#9ecae1':
+           bookCount > 1   ? '#c6dbef':
+                             '#eff3ff';
+}
 
-    // Add a marker to the map
-    L.marker([Latitude, Longitude]).addTo(map)
-        .bindPopup(popupContent);
-});
+function style(feature) {
+    let stateName = feature.properties.name;
+    let bookCount = banByState[stateName] ? banByState[stateName].bookCount : 0;
+    return {
+        fillColor: getColor(bookCount),
+        weight: 2,
+        opacity: 0.9,
+        color: 'white',
+        dashArray: '3',
+        fillOpacity: 0.75
+    };
+}
+
+L.geoJson(statesData, {style: style}).addTo(myMap);
+
+function init(banByState) {
+    let dropdownMenu = d3.select("#selDataset");
+    Object.keys(banByState).forEach(State => {
+        dropdownMenu.append("option")
+                    .text(State)
+                    .property("value", State);
+    });
+
+}
+
+
+
+
+
+
+
+
+
+
+// // JSON data containing latitude and longitude information
+// const jsonData = [
+//     {
+//         "Author": "Àbíké-Íyímídé, Faridah",
+//         "Latitude": 27.994402,
+//         "Longitude": -81.760254,
+//         "Title": "Ace of Spades",
+//         "Date_of_Challenge_or_Removal": 1635724800000,
+//         "District": "Indian River County School District",
+//         "Origin_of_Challenge": "Administrator",
+//         "State": "Florida",
+//         "Type_of_Ban": "Banned in Libraries and Classrooms"
+//         // Other properties...
+//     },
+//     // Other locations...
+// ];
+
+// // Initialize Leaflet map
+// const map = L.map('map').setView([37.8, -96], 4); // Centered on the US
+
+// // Add a tile layer to the map
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(map);
+
+// // Iterate over JSON data and add markers to the map for each location
+// jsonData.forEach(location => {
+//     const { Latitude, Longitude, Author, Title, Date_of_Challenge_or_Removal, District, Origin_of_Challenge, State, Type_of_Ban } = location;
+
+//     // Create a popup with information about the book
+//     const popupContent = `
+//         <b>${Title}</b><br>
+//         Author: ${Author}<br>
+//         Date of Challenge or Removal: ${new Date(Date_of_Challenge_or_Removal).toLocaleDateString()}<br>
+//         District: ${District}<br>
+//         Origin of Challenge: ${Origin_of_Challenge}<br>
+//         State: ${State}<br>
+//         Type of Ban: ${Type_of_Ban}
+//     `;
+
+//     // Add a marker to the map
+//     L.marker([Latitude, Longitude]).addTo(map)
+//         .bindPopup(popupContent);
+// });
